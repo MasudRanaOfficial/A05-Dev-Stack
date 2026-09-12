d7fc9104b0b5c535833ef1f9e4f84d9aca547a69
Masud Rana <masudrana.arh@gmail.com>
2026-09-12 13:30:17 +0600

    init project with vite, tailwind, and daisyui

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="text-4xl text-red-300">Hi I am Masud</h1>
+      <button className="btn btn-neutral">Neutral</button>
+    </>
+  );
+}
+
+export default App
